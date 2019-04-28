04279437215d6d531c5cceedabd28a2dee9b4666
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-28 21:38:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,30 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    
+    get '/' do 
+      erb :root
+     
+    end 
+    
+    get '/new' do 
+     erb :'pirates/new'
+    end 
+    
+ post '/show' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+       Ship.new(details)
+      end
+     
+
+      erb :'pirates/show'
+
+      @ships = Ship.all
+    end
+
+    
 
   end
 end
